4972c39b1a52883960939a70e13efe27a7c80d05
khalid <77464672+f5rcbh@users.noreply.github.com>
2023-06-25 19:22:10 +0300

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,4 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({
-    site: 'https://f5rcbh.github.io',
-    base: '/News-homePage'
-});
+export default defineConfig({});
